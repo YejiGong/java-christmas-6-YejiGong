6afced057ed667e78183c2c04508c2928b3d7c5b
YejiGong <yejigong00@gmail.com>
2023-11-12 22:04:25 +0900

    Test: OutputView 테스트코드 작성

--- src/test/java/christmas/View/OutputViewTest.java
+++ src/test/java/christmas/View/OutputViewTest.java
@@ -1,5 +1,43 @@
 package christmas.View;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class OutputViewTest {
+    static OutputView outputView = new OutputView();
+    static OutputStream out;
+
+    @BeforeEach
+    void setUp() {
+        out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+    }
+
+    @DisplayName("출력 테스트")
+    @Test
+    void printTest() {
+        outputView.print("any string");
+        Assertions.assertThat(out.toString().trim()).contains("any string");
+    }
+
+    @DisplayName("리스트 출력 테스트")
+    @Test
+    void printMultipleLineTest() {
+        outputView.printMultipleLine(List.of("타파스 1개", "제로콜라 1개"));
+        Assertions.assertThat(out.toString().trim()).contains("타파스 1개", "제로콜라 1개");
+    }
+
+    @DisplayName("금액 출력 테스트")
+    @Test
+    void printMoneyTest() {
+        outputView.printMultipleLine(List.of(8500));
+        Assertions.assertThat(out.toString().trim()).contains("8,500원");
+    }
 
 }
